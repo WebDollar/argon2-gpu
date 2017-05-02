ba257af4bff454248b965938c23e94a97e015929
Ondrej Mosnáček <omosnacek@gmail.com>
2017-05-02 22:48:28 +0200

    [test] Add CLI to testing tool

--- src/argon2-gpu-test/main.cpp
+++ src/argon2-gpu-test/main.cpp
@@ -1,3 +1,6 @@
+#include "commandline/commandlineparser.h"
+#include "commandline/argumenthandlers.h"
+
 #include "argon2-gpu-common/argon2params.h"
 #include "argon2-opencl/processingunit.h"
 #include "argon2-cuda/processingunit.h"
@@ -83,11 +86,30 @@ std::size_t runTests(const GlobalContext &global, const Device &device,
 
 template<class Device, class GlobalContext,
          class ProgramContext, class ProcessingUnit>
-void runAllTests(std::size_t &failures)
+int runAllTests(const char *progname, const char *name, std::size_t deviceIndex,
+                bool listDevices, std::size_t &failures)
 {
     GlobalContext global;
     auto &devices = global.getAllDevices();
-    auto &device = devices[0];
+
+    if (listDevices) {
+        for (std::size_t i = 0; i < devices.size(); i++) {
+            auto &device = devices[i];
+            std::cout << "Device #" << i << ": " << device.getInfo()
+                      << std::endl;
+        }
+        return 0;
+    }
+
+    if (deviceIndex >= devices.size()) {
+        std::cerr << progname << ": Device index out of range!" << std::endl;
+        return 2;
+    }
+
+    auto &device = devices[deviceIndex];
+    std::cout << "Running " << name << " tests..." << std::endl;
+    std::cout << "Using device #" << deviceIndex << ": " << device.getName()
+              << std::endl;
 
     failures += runTests<Device, GlobalContext, ProgramContext, ProcessingUnit>
             (global, device, ARGON2_I, ARGON2_VERSION_10,
@@ -107,29 +129,91 @@ void runAllTests(std::size_t &failures)
     failures += runTests<Device, GlobalContext, ProgramContext, ProcessingUnit>
             (global, device, ARGON2_ID, ARGON2_VERSION_13,
              ARRAY_BEGIN(CASES_ID_13), ARRAY_END(CASES_ID_13));
+    return 0;
 }
 
-int main(void) {
+using namespace libcommandline;
+
+struct Arguments
+{
+    bool showHelp = false;
+    bool listDevices = false;
+
+    std::string mode = "cuda";
+
+    std::size_t deviceIndex = 0;
+};
+
+static CommandLineParser<Arguments> buildCmdLineParser()
+{
+    static const auto positional = PositionalArgumentHandler<Arguments>(
+                [] (Arguments &, const std::string &) {});
+
+    std::vector<const CommandLineOption<Arguments>*> options {
+        new FlagOption<Arguments>(
+            [] (Arguments &state) { state.listDevices = true; },
+            "list-devices", 'l', "list all available devices and exit"),
+
+        new ArgumentOption<Arguments>(
+            [] (Arguments &state, const std::string &mode) { state.mode = mode; },
+            "mode", 'm', "mode in which to run ('cuda' for CUDA or 'opencl' for OpenCL)", "cuda", "MODE"),
+
+        new ArgumentOption<Arguments>(
+            makeNumericHandler<Arguments, std::size_t>([] (Arguments &state, std::size_t index) {
+                state.deviceIndex = (std::size_t)index;
+            }), "device", 'd', "use device with index INDEX", "0", "INDEX"),
+
+        new FlagOption<Arguments>(
+            [] (Arguments &state) { state.showHelp = true; },
+            "help", '?', "show this help and exit")
+    };
+
+    return CommandLineParser<Arguments>(
+        "A tool for testing the argon2-opencl and argon2-cuda libraries.",
+        positional, options);
+}
+
+int main(int, const char * const *argv) {
+    CommandLineParser<Arguments> parser = buildCmdLineParser();
+
+    Arguments args;
+    int ret = parser.parseArguments(args, argv);
+    if (ret != 0) {
+        return ret;
+    }
+    if (args.showHelp) {
+        parser.printHelp(argv);
+        return 0;
+    }
+
     std::size_t failures = 0;
-
-    std::cout << "Running CUDA tests..." << std::endl;
-    try {
-        runAllTests<cuda::Device, cuda::GlobalContext, cuda::ProgramContext,
-                cuda::ProcessingUnit>(failures);
-    } catch (cuda::CudaException &err) {
-        std::cerr << "CUDA ERROR: " << err.what() << std::endl;
+    if (args.mode == "cuda") {
+        try {
+            ret = runAllTests<cuda::Device, cuda::GlobalContext,
+                    cuda::ProgramContext, cuda::ProcessingUnit>(
+                        argv[0], "CUDA", args.deviceIndex, args.listDevices,
+                        failures);
+        } catch (cuda::CudaException &err) {
+            std::cerr << argv[0] << ": CUDA ERROR: " << err.what() << std::endl;
+            return 2;
+        }
+    } else if (args.mode == "opencl") {
+        try {
+            ret = runAllTests<opencl::Device, opencl::GlobalContext,
+                    opencl::ProgramContext, opencl::ProcessingUnit>(
+                        argv[0], "OpenCL", args.deviceIndex, args.listDevices,
+                        failures);
+        } catch (cl::Error &err) {
+            std::cerr << argv[0] << ": OpenCL ERROR: " << err.err() << ": "
+                      << err.what() << std::endl;
+            return 2;
+        }
+    } else {
+        std::cerr << argv[0] << ": invalid mode: " << args.mode << std::endl;
         return 2;
     }
-
-    std::cout << "Running OpenCL tests..." << std::endl;
-    try {
-        runAllTests<opencl::Device, opencl::GlobalContext, opencl::ProgramContext,
-                opencl::ProcessingUnit>(failures);
-    } catch (cl::Error &err) {
-        std::cerr << "OpenCL ERROR: " << err.err() << ": "
-                  << err.what() << std::endl;
-        return 2;
-    }
+    if (ret)
+        return ret;
 
     if (failures) {
         std::cout << failures << " TESTS FAILED!" << std::endl;
