aa584b5943af1992a59b75fe300d02676c2ea4fc
Ondrej Mosnáček <omosnacek@gmail.com>
2017-05-02 22:46:53 +0200

    [scripts] Improve naming of devices

--- scripts/gpu_vs_cpu.R
+++ scripts/gpu_vs_cpu.R
@@ -8,7 +8,7 @@ NS_PER_SEC <- 1000 * 1000 * 1000
 
 theme_set(theme_gray(base_size = 10))
 
-GRAPH_WIDTH_CM <- 10
+GRAPH_WIDTH_CM <- 12
 GRAPH_HEIGHT_CM <- 6
 
 save_graph <- function(name, plot) {
@@ -19,7 +19,7 @@ args <- commandArgs(trailingOnly = TRUE)
 
 machines <- read.csv('machines.csv', row.names=NULL)
 
-machines$id <-paste0(machines$type, '-', machines$name)
+machines$id <- paste0(machines$type, '-', machines$id)
 
 data <- data.frame()
 
@@ -36,7 +36,7 @@ for (i in 1:nrow(machines)) {
     mode <- 'cuda'
   }
   
-  file <- paste0('bench-', type, '-', name, '-', branch, '.csv')
+  file <- paste0('bench-', id, '-', branch, '.csv')
   if (file.exists(file)) {
     data_file <- read.csv(file, row.names=NULL)
     data_file <- data_file[data_file$mode == mode & data_file$kernel == 'by-segment',]
@@ -44,7 +44,7 @@ for (i in 1:nrow(machines)) {
       data_file <- data_file[data_file$precompute == 'yes' | data_file$type == 'Argon2d',]
     }
     
-    data <- rbind(data, data.frame(Machine=id, HW=toupper(type), Version=data_file$version, Type=data_file$type, t_cost=data_file$t_cost, m_cost=data_file$m_cost, lanes=data_file$lanes, ns_per_hash=data_file$ns_per_hash, power=power))
+    data <- rbind(data, data.frame(Machine=id, Name=name, HW=toupper(type), Version=data_file$version, Type=data_file$type, t_cost=data_file$t_cost, m_cost=data_file$m_cost, lanes=data_file$lanes, ns_per_hash=data_file$ns_per_hash, power=power))
   }
 }
 
@@ -68,14 +68,14 @@ make_plots_bps <- function(type) {
     prefix <- paste0('plot-bps-argon2', type)
 
     save_graph(paste0(prefix, '-t_cost'),
-               ggplot(data_b[data_b_f_m_cost,], aes(x=t_cost, y=blocks_per_second, group=Machine, colour=Machine)) +
+               ggplot(data_b[data_b_f_m_cost,], aes(x=t_cost, y=blocks_per_second, colour=Name)) +
                  geom_line() +
                  scale_y_continuous(labels=comma) +
                  facet_grid(lanes~m_cost, labeller=label_both) +
                  xlab('t_cost') + ylab('Blocks per second'))
     
     save_graph(paste0(prefix, '-m_cost'),
-               ggplot(data_b[data_b_f_t_cost,], aes(x=m_cost, y=blocks_per_second, group=Machine, colour=Machine)) +
+               ggplot(data_b[data_b_f_t_cost,], aes(x=m_cost, y=blocks_per_second, colour=Name)) +
                  geom_line() +
                  scale_x_log10() +
                  scale_y_continuous(labels=comma) +
@@ -83,7 +83,7 @@ make_plots_bps <- function(type) {
                  xlab('m_cost (log scale)') + ylab('Blocks per second'))
     
     save_graph(paste0(prefix, '-lanes'),
-               ggplot(data_b[data_b_f_t_cost & data_b_f_m_cost,], aes(x=lanes, y=blocks_per_second, group=Machine, colour=Machine)) +
+               ggplot(data_b[data_b_f_t_cost & data_b_f_m_cost,], aes(x=lanes, y=blocks_per_second, colour=Name)) +
                  geom_line() +
                  scale_y_continuous(labels=comma) +
                  facet_grid(t_cost~m_cost, labeller=label_both) +
@@ -101,14 +101,14 @@ make_plots_bpj <- function(type) {
     prefix <- paste0('plot-bpj-argon2', type)
     
     save_graph(paste0(prefix, '-t_cost'),
-               ggplot(data_b[data_b_f_m_cost,], aes(x=t_cost, y=blocks_per_joule, group=Machine, colour=Machine)) +
+               ggplot(data_b[data_b_f_m_cost,], aes(x=t_cost, y=blocks_per_joule, colour=Name)) +
                  geom_line() +
                  scale_y_continuous(labels=comma) +
                  facet_grid(lanes~m_cost, labeller=label_both) +
                  xlab('t_cost') + ylab('Blocks per joule'))
     
     save_graph(paste0(prefix, '-m_cost'),
-               ggplot(data_b[data_b_f_t_cost,], aes(x=m_cost, y=blocks_per_joule, group=Machine, colour=Machine)) +
+               ggplot(data_b[data_b_f_t_cost,], aes(x=m_cost, y=blocks_per_joule, colour=Name)) +
                  geom_line() +
                  scale_x_log10() +
                  scale_y_continuous(labels=comma) +
@@ -116,7 +116,7 @@ make_plots_bpj <- function(type) {
                  xlab('m_cost (log scale)') + ylab('Blocks per joule'))
     
     save_graph(paste0(prefix, '-lanes'),
-               ggplot(data_b[data_b_f_t_cost & data_b_f_m_cost,], aes(x=lanes, y=blocks_per_joule, group=Machine, colour=Machine)) +
+               ggplot(data_b[data_b_f_t_cost & data_b_f_m_cost,], aes(x=lanes, y=blocks_per_joule, colour=Name)) +
                  geom_line() +
                  scale_y_continuous(labels=comma) +
                  facet_grid(t_cost~m_cost, labeller=label_both) +
